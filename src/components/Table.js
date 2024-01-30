86aea6b63cb938fb0b4353b7464391ba7abf0965
alecascencio <mellamoaleks@gmail.com>
2024-01-29 19:08:21 -0600

    first commit

---
+++ src/components/Table.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Table.css';
+
+export const Table = () => {
+  return (
+    <div>
+      <thead>
+        <tbody>
+          <tr>
+            <th>Page</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </tbody>
+      </thead>
+    </div>
+  );
+};
