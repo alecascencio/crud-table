5baab51505c6aefb9dfaf3b8aa0c3d6c5c97cf8a
alecascencio <mellamoaleks@gmail.com>
2024-01-29 21:05:09 -0600

    Modal

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,19 +1,61 @@
 import React from 'react';
+import { BsFillTrashFill, BsFillPencilFill } from 'react-icons/bs';
 import './Table.css';
 
 export const Table = () => {
   return (
-    <div>
-      <thead>
-        <tbody>
+    <div className='table-wrapper'>
+      <table className='table'>
+        <thead>
           <tr>
             <th>Page</th>
-            <th>Description</th>
+            <th className='expand'>Description</th>
             <th>Status</th>
             <th>Action</th>
           </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Home</td>
+            <td>This the main page</td>
+            <td>
+              <span className='label label-live'>Live</span>
+            </td>
+            <td>
+              <span className='actions'>
+                <BsFillTrashFill className='delete-btn' />
+                <BsFillPencilFill />
+              </span>
+            </td>
+          </tr>
+          <tr>
+            <td>Page 2</td>
+            <td>This the second page</td>
+            <td>
+              <span className='label label-draft'>Draft</span>
+            </td>
+            <td>
+              <span className='actions'>
+                <BsFillTrashFill className='delete-btn' />
+                <BsFillPencilFill />
+              </span>
+            </td>
+          </tr>
+          <tr>
+            <td>Page 3</td>
+            <td>This the third page</td>
+            <td>
+              <span className='label label-error'>Draft</span>
+            </td>
+            <td>
+              <span className='actions'>
+                <BsFillTrashFill className='delete-btn' />
+                <BsFillPencilFill />
+              </span>
+            </td>
+          </tr>
         </tbody>
-      </thead>
+      </table>
     </div>
   );
 };
