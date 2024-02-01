35d09d3dca50ea7fc49314ca8fc0e9e2889f57ea
kadabra <mellamoaleks@gmail.com>
2024-02-01 13:01:24 -0600

    Checkbox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { useState } from 'react';
-
 import './App.css';
+import { useState } from 'react';
 import { Modal } from './components/Modal';
 import { Table } from './components/Table';
 
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,13 +1,59 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { BsFillTrashFill, BsFillPencilFill } from 'react-icons/bs';
 import './Table.css';
 
 export const Table = ({ rows, deleteRow, editRow }) => {
+  const [selectedRows, setSelectedRows] = useState([]);
+  const checkboxref = useRef(null);
+
+  const toggleSelectAll = (e) => {
+    if (e.target.checked) {
+      const ids = rows.map((row, idx) => {
+        return idx;
+      });
+
+      setSelectedRows(ids);
+    } else {
+      setSelectedRows([]);
+    }
+  };
+
+  const toggleRowSelection = (idx) => {
+    let newSelectedRows;
+
+    if (selectedRows.includes(idx)) {
+      newSelectedRows = selectedRows.filter(
+        (selectedIdx) => selectedIdx !== idx
+      );
+    } else {
+      newSelectedRows = [...selectedRows, idx];
+    }
+
+    setSelectedRows(newSelectedRows);
+  };
+
+  useEffect(() => {
+    if (selectedRows.length === 0) {
+      checkboxref.current.checked = false;
+    }
+
+    if (selectedRows.length === rows.length) {
+      checkboxref.current.checked = true;
+    }
+  }, [selectedRows]);
+
   return (
     <div className='table-wrapper'>
       <table className='table'>
         <thead>
           <tr>
+            <th>
+              <input
+                type='checkbox'
+                ref={checkboxref}
+                onChange={toggleSelectAll}
+              />
+            </th>
             <th>ID</th>
             <th>Page</th>
             <th className='expand'>Description</th>
@@ -22,6 +68,14 @@ export const Table = ({ rows, deleteRow, editRow }) => {
 
             return (
               <tr key={idx}>
+                <td>
+                  <input
+                    type='checkbox'
+                    id={`checkbox-${idx}`}
+                    checked={selectedRows.includes(idx)}
+                    onChange={() => toggleRowSelection(idx)}
+                  />
+                </td>
                 <td>{idx + 1}</td>
                 <td>{row.page}</td>
                 <td className='expand'>{row.description}</td>
